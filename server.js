b855ef8ca4839b942dd2bdeabd4ec27a42416324
Anna Piwnik <a.piwnik@o2.pl>
2018-04-09 20:22:54 +0200

    Finish task

--- server.js
+++ server.js
@@ -8,21 +8,20 @@ app.use(bodyParser.json());
 
 app.get('/getNote', function (req, res) {
 
-fs.readFile('./test.json', 'utf8', function(err, data) {
-    if (err) throw err;
-    stringifyFile = data
-    res.send(data);
-});
+	fs.readFile('./test.json', 'utf8', function(err, data) {
+	    if (err) throw err;
+	    stringifyFile = data
+	    res.send(data);
+	});
 });
 
-app.post('updateNote/:note', function (req, res) {
-
-fs.writeFile('./test.json', stringifyFile, function(err, data) {
-    if (err) throw err;
-    console.log('file updated');
-    stringifyFile = req.params.note 
-    res.send(req.params.note); 
-});
+app.post('/updateNote/:note', function (req, res) {
+	stringifyFile += req.params.note;
+	fs.writeFile('./test.json', stringifyFile, function(err) {
+	    if (err) throw err;
+	    console.log('file updated');
+	    res.send(stringifyFile); 
+	});
 });
 
 app.listen(3000);
